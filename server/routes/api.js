0d03204c1d815732efdc5b6ab681ad53d8e7e94b
antonio.barrantes <tonbarramon@gmail.com>
2021-10-13 01:08:30 +0200

    Validaciones al crear producto

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,32 +1,31 @@
 import express from "express";
+import {validateCreateProduct} from '../validators/product.js';
 
-import { readAllProducts, createProduct, readProduct, readProductsFiltered, updateProduct, deleteProduct } from "../controllers/api.js";
+import {
+  readAllProducts,
+  createProduct,
+  readProduct,
+  readProductsFiltered,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/api.js";
 
 const router = express.Router();
 
 // http://localhost:5000/api/product/...
 
 // Create Product
-router.post('/product/create/', createProduct);
+router.post("/product/create/", validateCreateProduct, createProduct);
 
 // Read Product
 router.get("/product/:id", readProduct);
 router.get("/product/", readAllProducts);
-router.get('/product/filter/:filter', readProductsFiltered);
+router.get("/product/filter/:filter", readProductsFiltered);
 
 // Update Product
-router.put('/product/update/:id', updateProduct);
+router.put("/product/update/:id", updateProduct);
 
 // Delete Product
-router.delete('/product/delete/:id', deleteProduct);
+router.delete("/product/delete/:id", deleteProduct);
 
 export default router;
-
-/*
- {
-    "name": "peluche",
-    "categories": ["tech","house"],
-    "exchange": "present",
-    "state": "available"
-}
- */

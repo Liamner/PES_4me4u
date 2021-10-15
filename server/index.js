f735ea661b63dacac33b667354fcb4ada0e7d844
T0nete <tonbarramon@gmail.com>
2021-10-15 14:54:37 +0200

    update package json

--- server/index.js
+++ server/index.js
@@ -16,15 +16,6 @@ app.use("/api", apiRoutes);
 const CONNECTION_URL =
   "mongodb+srv://Jefe:1234@4me4u.iyl4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-//static files location
-// Exprees will serve up production assets
-app.use(express.static(path.join(__dirname, '../app')));
-
-// Express serve up index.html file if it doesn't recognize route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../app/App.tsx'));
-});
-
 app.use(express.static(__dirname+'/public'));
 
 const PORT = process.env.PORT || 5000;

b070ef19c820acb631863cf20ab332690ee1761d
antonio.barrantes <tonbarramon@gmail.com>
2021-11-04 16:56:02 +0100

    no message

--- server/routes/api.js
+++ server/routes/api.js
@@ -80,7 +80,6 @@ module.exports = function(app) {
     .get(imageController.getProductImages)
     .post(upload.array('img',6), imageController.uploadImages)
     .delete(imageController.deleteImages)
-    .put(upload.array('img',6) imageController.updateImages)
 
     
 

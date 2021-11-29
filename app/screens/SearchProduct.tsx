fe355362016a146f4fb15abfa4b99f2ce0c579d0
Liamner <nuria.perez.olivares@gmail.com>
2021-11-29 09:12:44 +0100

    Intento 2

--- app/screens/SearchProduct.tsx
+++ app/screens/SearchProduct.tsx
@@ -7,13 +7,29 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function ProductSearch({ navigation }: RootTabScreenProps<'ProductSearch'>) {
+
+  const [products, setProducts] = useState();
+  const [text, onChangeText] = useState("");
+
+  const getPNameInfo = async () => {
+
+    let response = await axios.get('https://app4me4u.herokuapp.com/api/product/name/'+text);
+    
+  };
 	
   return (
     <View style={styles.container}>
-      <TextInput
-				onChangeText={()=>console.log("Texto cambiado")}
-				placeholder="Buscar..."
-			/>
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Buscar..."
+        />
+        <Button
+          onPress={getPNameInfo}
+          title="Buscar"
+        />
+      </View>
 			<View style={styles.row}>
 				<Button
 					onPress={()=>console.log("Buscar por categoria")}

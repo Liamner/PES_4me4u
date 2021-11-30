04466b2353afd9c71aa187ed78b349f748020f45
antonio.barrantes <tonbarramon@gmail.com>
2021-11-30 16:48:04 +0100

    no message

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 require('./config/config.js');
 require('dotenv').config()
@@ -15,8 +14,6 @@ app.use(bodyParser.json())
 
 const apiRoutes =  require('./routes/api.js')(app);
 app.use("/api", apiRoutes);
-
-//app.use(cors());
 
 
 const CONNECTION_URL =

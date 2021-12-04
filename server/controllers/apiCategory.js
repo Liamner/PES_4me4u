6593574d753a84c349cb8adc4b718304fd61b486
T0nete <tonbarramon@gmail.com>
2021-12-04 19:23:20 +0100

    Filtrar por categorias y tipo a la vez

--- server/controllers/apiCategory.js
+++ server/controllers/apiCategory.js
@@ -66,15 +66,34 @@ exports.getProductCategory = async (req, res) => {
   
   console.log('Reading category: ' + req.body.category);
   const category = req.body.category;
+  let exchangeType;
+  if (req.body.exchange != null) exchangeType = req.body.exchange;
+  else exchangeType = null;
 
+  console.log(exchangeType)
   try {
     //const products = await Category.find({name: category},  {products: 1 }).populate('products').populate('exchange');
     //const products = await Category.find({name: category},  {products: 1 })//.populate('products');
+    let result = [];
     if (category != null) {
       Category.find({name: category}, {products: 1}, async (erro, products) => {
-        console.log(products[0]._id)
-        res.status(200).json(products);
-      }).populate('products')
+        console.log(products[0].products.length)
+       
+        if (exchangeType != null) {
+          for (let i = 0; i < products[0].products.length; i++) {
+            if (products[0].products[i].exchange[0].name == exchangeType) {
+              console.log('igual')
+              result.push(products[0].products[i])
+            }
+            //console.log(products[0].products[i].exchange[0].name)
+          }
+          res.status(200).json(result);
+        }
+        else {
+          res.status(200).json(products);
+        }
+        
+      }).populate({path: 'products',populate: {path: 'exchange', select: { 'name': 1}}})
     }
     
    
--- server/controllers/apiProduct.js
+++ server/controllers/apiProduct.js
@@ -117,13 +117,12 @@ exports.createProduct = async (req, res) => {
     
     const newProduct = await product.save();
     // Add the product to the user 
-    //jo crec que aixo no funciona
-    const user = await User.findByIdAndUpdate(
+    /*const user = await User.findByIdAndUpdate(
                             { _id: ObjectId(req.user.id) }, 
                               {$push : {
                                 products: newProduct
                               }
-                            });
+                            });*/
 
     const categories = await Category.findByIdAndUpdate(
                             { _id: ObjectId(req.body.categories) }, 

370818c14bcbbf9c67d1b6eeccca196396fb5e52
p-kami-sama <paul.gonzalez@estudiantat.upc.edu>
2021-12-08 19:53:40 +0100

    Nu funciona...sos :(

--- app/screens/ProductEdit.tsx
+++ app/screens/ProductEdit.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { Button, Platform,ScrollView, Image, StyleSheet,Modal, Dimensions, FlatList, Pressable, TouchableOpacity, Alert } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import EditScreenInfo from '../components/EditScreenInfo';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import * as ImagePicker from 'expo-image-picker';
 import { Text, View } from '../components/Themed';
 import { TextInput, Checkbox } from 'react-native-paper';
 import { RootTabScreenProps } from '../types';
 import { resolvePlugin } from '@babel/core';
+import { State } from 'react-native-gesture-handler';
 
 export default function EditProduct({ navigation }: RootTabScreenProps<'EditProduct'>) {
   const [name, onChangeName] = React.useState("");
@@ -17,6 +18,8 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
   const [checkedDonar, setCheckedDonar] = React.useState(false);
   const [checkedIntercambiar, setCheckedIntercambiar] = React.useState(false);
   const [checkedPrestar, setCheckedPrestar] = React.useState(false);
+  
+  const [numImages, setNumImages] = React.useState();     //número de imagenes, para controlar que por lo menos hay una
 
   const [image, setImage] = React.useState(null);
   const [image2, setImage2] = React.useState(null);
@@ -25,10 +28,24 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
   const [image5, setImage5] = React.useState(null);
   const [image6, setImage6] = React.useState(null); 
 
+  const [oldIds, setOldIds] = React.useState([]);   //necesarios para borrar, imagenes, elegir cual de ellos borrar  
+
+//para controlar si se cambia o no
+  const [imageMod, setImageMod] = React.useState("no");
+  const [image2Mod, setImage2Mod] = React.useState("no");
+  const [image3Mod, setImage3Mod] = React.useState("no");
+  const [image4Mod, setImage4Mod] = React.useState("no");
+  const [image5Mod, setImage5Mod] = React.useState("no");
+  const [image6Mod, setImage6Mod] = React.useState("no"); 
+
+
   //copias auxiliares en caso de recargar el producto inicial
   const [checkedDonarAux, setCheckedDonarAux] = React.useState(false);
   const [checkedIntercambiarAux, setCheckedIntercambiarAux] = React.useState(false);
   const [checkedPrestarAux, setCheckedPrestarAux] = React.useState(false);
+
+  const [numImagesAux, setNumImagesAux] = React.useState();     //número de imagenes inicial
+
 
   const [imageAux, setImageAux] = React.useState(null);
   const [image2Aux, setImage2Aux] = React.useState(null);
@@ -86,35 +103,51 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
 
 
   //AÑADIR FOTOS
-  setProductInfo({
-    ...productInfo,
-    pimage: response.data.img[0].url });
 
+//    numImages =  (response.data.img).length;
+    setNumImages((response.data.img).length);
+    setNumImagesAux((response.data.img).length);
 
     if((response.data.img).length >= 1){
       setImage(response.data.img[0].url);
       setImageAux(response.data.img[0].url);
+
+      oldIds.push(response.data.img[0]._id);
     }
     if((response.data.img).length >= 2){
       setImage2(response.data.img[1].url);
       setImage2Aux(response.data.img[1].url);
+
+      oldIds.push(response.data.img[1]._id);
+
     }
     if((response.data.img).length >= 3){
       setImage3(response.data.img[2].url);
       setImage3Aux(response.data.img[2].url);
+     
+      oldIds.push(response.data.img[2]._id);
+
     }
     if((response.data.img).length >= 4){
       setImage4(response.data.img[3].url);
       setImage4Aux(response.data.img[3].url);
+
+      oldIds.push(response.data.img[3]._id);
     }
     if((response.data.img).length >= 5){
       setImage5(response.data.img[4].url);
       setImage5Aux(response.data.img[4].url);
+
+      oldIds.push(response.data.img[4]._id);
     }
     if((response.data.img).length >= 6){
       setImage6(response.data.img[5].url);
       setImage6Aux(response.data.img[5].url);
+
+      oldIds.push(response.data.img[5]._id);
+
     }
+    console.log(oldIds);
   };
 
   //recargar el producto sin editar
@@ -134,12 +167,54 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
     setImage5(image5Aux)
     setImage6(image6Aux)
 
+    setNumImages(numImagesAux);
+  
+
     console.log(productInfo.pname + ' reloaded')
     console.log(productInfo.pdescription + ' reloaded')
     console.log(productInfo.pexchange[0] + ' reloaded')
   }
 
-  
+  const editImages = async () => {
+
+
+    //DELETE pasar vector de ids
+    //NOTA: no se pueden borrar todas las fotos, pasa cuando numImages == numImagesAux o 
+    let oldId = [];
+    //let oldId: string | never[] | AxiosRequestConfig<any> | undefined = [];
+    if ((image  == null && image  != null) || image != imageAux  ) oldId = oldId + image;
+    if ((image2 == null && image2 != null) || image2 != image2Aux) oldId = oldId + image2;
+    if ((image3 == null && image3 != null) || image3 != image3Aux) oldId = oldId + image3;
+    if ((image4 == null && image4 != null) || image4 != image4Aux) oldId = oldId + image4;
+    if ((image5 == null && image5 != null) || image5 != image5Aux) oldId = oldId + image5;
+    if ((image6 == null && image6 != null) || image6 != image6Aux) oldId = oldId + image6;
+
+    await axios
+      .delete('https://app4me4u.herokuapp.com/api/image/' + pid, oldId)
+      .then(function(response) {
+        console.log("Old images deleted")
+      })
+      .catch(function(error) {
+        console.log(error);
+    });
+
+    //POST pasar url nuevas
+    let newUrls = [image, image2, image3, image4, image5, image6];
+
+    await axios
+      .post('https://app4me4u.herokuapp.com/api/image/' + pid, newUrls)
+      .then(function(response) {
+        console.log("New images posted")
+      })
+      .catch(function(error) {
+        console.log(error);
+    });
+
+  }
+
+
+
+
   const editProduct = async () => {
     let ex = [];
     if (checkedDonar) {
@@ -152,12 +227,12 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
       ex.push('provide')
     }
     const newInfo = {
-      name:name,
+      name: name,
       categories: selectedCategory,
       description: description,
-      exchange: ex,
-      img: [image, image2, image3, image4, image5, image6]
+      exchange: ex      
     };
+
     await axios
       .put('https://app4me4u.herokuapp.com/api/product/update/' + pid, newInfo)
       .then(function(response) {
@@ -169,20 +244,17 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
           pname: result.name,
           pcategories: result.categories,
           pdescription: result.description,
-          pexchange: result.exchange,
-          pimage:  result.img[0].url,
-          pimage2: result.img[1].url,
-          pimage3: result.img[2].url,
-          pimage4: result.img[3].url,
-          pimage5: result.img[4].url,
-          pimage6: result.img[5].url
+          pexchange: result.exchange
         });
         
       })
       .catch(function(error) {
         console.log(error);
+
     });
 
+
+    editImages();
   }
    
   React.useEffect(() => {
@@ -195,7 +267,7 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
       }
     })();
   }, []);  
-  const setImageById = (id: Number, uri: string) => {      
+  const setImageById = (id: Number, uri: string) => {
       if(uri != ''){
         if(id == 1 ) setImage(uri)
         if(id == 2) setImage2(uri)
@@ -214,29 +286,49 @@ export default function EditProduct({ navigation }: RootTabScreenProps<'EditProd
       }
   }
   const unPickImage = async (id: Number) => {
+    console.log(numImages);
     Alert.alert(
   '¿Que quieres hacer con tu foto?', '',
-  [    
+  [
     {
       text: 'Eliminar foto',
       onPress: () => {
-        setImageById(id, '');
+        if (numImages == 1){
+          Alert.alert(
+            "Error",
+            "No se puede borrar la última fotografia de un producto.",
+            [{ text: "OK", onPress: () => console.log("Error al intentar borrar la última imagen de un producto.") }
+            ]);
+        }
+        else{
+          setNumImages(numImages -1);
+          setImageById(id, '');
+        }
       },
       style: 'default',
     },
     {
       text: 'Hacer una foto', 
       onPress: () => {
-        setImageById(id, '');
-        pickImage(id);
+
+        //setImageById(id, '');
+        pickImage(id, true);
       }
     },
   ],
   {cancelable: false},
-);  
-    
+  
+);
+  console.log(numImages);
+
   }
-const pickImage = async (id?: Number) => {    
+
+
+
+const pickImage = async (id?: Number, change?: Boolean) => {
+  setNumImages(numImages +1);
+
+
   let result = await ImagePicker.launchImageLibraryAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.All,
     allowsEditing: true,
@@ -245,16 +337,32 @@ const pickImage = async (id?: Number) => {
   });
 
   console.log(result);
-
   if (!result.cancelled ) {
-    if(!image || id == 1)setImage(result.uri);
-    else if(!image2 || id == 2)setImage2(result.uri);
-    else if(!image3 || id == 3)setImage3(result.uri);
-    else if(!image4 || id == 4)setImage4(result.uri);
-    else if(!image5 || id == 5)setImage5(result.uri);
-    else if(!image6 || id == 6)setImage6(result.uri);
-    
+    setImageById(id, '');
+
+    if (id == 1){
+        setImage(result.uri);
+    } 
+    else if(id == 2){
+      setImage2(result.uri);
+    }
+    else if(id == 3){
+      setImage3(result.uri);
+    }
+    else if(id == 4){
+      setImage4(result.uri);
+    }
+    else if(id == 5){
+      setImage5(result.uri);
+    }
+    else if(id == 6){
+      setImage6(result.uri);
+    }  
   }
+  else {
+    setNumImages(numImages -1);
+  }
+
 };
 
   return (
@@ -300,11 +408,6 @@ const pickImage = async (id?: Number) => {
 
 
 
-
-
-          {//AÑADIR EL RESTO DE CATEGORIAS
-          }
-
         </Picker>        
         <Text style={[styles.title, {marginTop:20}]}> ¿Que quieres hacer con tu producto?</Text>        
         <View 
@@ -348,7 +451,7 @@ const pickImage = async (id?: Number) => {
             <Image style={styles.image} source={{ uri: image }} />
           </TouchableOpacity>  }          
           {!image && 
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(1)}>
             <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
           </TouchableOpacity>  }
           {image2 && 
@@ -356,7 +459,7 @@ const pickImage = async (id?: Number) => {
           <Image style={styles.image} source={{ uri: image2 }} />
           </TouchableOpacity>  }  
           {!image2 && 
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(2)}>
             <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
           </TouchableOpacity>  }
           {image3 && 
@@ -364,7 +467,7 @@ const pickImage = async (id?: Number) => {
           <Image style={styles.image} source={{ uri: image3 }} />
           </TouchableOpacity>  }  
           {!image3 && 
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(3)}>
           <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
         </TouchableOpacity>  }        
         </ View>
@@ -378,7 +481,7 @@ const pickImage = async (id?: Number) => {
           <Image style={styles.image} source={{ uri: image4 }} />
           </TouchableOpacity>}
           {!image4 && 
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(4)}>
           <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
           </TouchableOpacity>  }
           {image5 && 
@@ -386,7 +489,7 @@ const pickImage = async (id?: Number) => {
           <Image style={styles.image} source={{ uri: image5 }} />
           </TouchableOpacity>}
           {!image5 &&
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(5)}>
           <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
           </TouchableOpacity>  }
           {image6 && 
@@ -394,7 +497,7 @@ const pickImage = async (id?: Number) => {
           <Image style={styles.image} source={{ uri: image6 }} />
           </TouchableOpacity>}
           {!image6 && 
-          <TouchableOpacity style={styles.notImage} onPress={pickImage}>
+          <TouchableOpacity style={styles.notImage} onPress={() => pickImage(6)}>
           <Image source={require('../images/camara2.png')}  style={styles.cameraImage} />  
           </TouchableOpacity>  }
         </View>    

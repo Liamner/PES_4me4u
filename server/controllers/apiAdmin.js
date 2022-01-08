641ae852704417daf8377d74838ff8d9ab29348f
adp99 <ana.diaz.poveda@estudiantat.upc.edu>
2022-01-08 01:04:18 +0100

    increase blockedusers

--- server/controllers/apiAdmin.js
+++ server/controllers/apiAdmin.js
@@ -256,4 +256,19 @@ exports.readLoans =  async (req, res) => {
     }
   };
 
+  exports.increaseBlockedUsers =  async (req, res) => {
+    try {
+      const adminId = req.params.id;
+      const admin = await Admin.findById({_id: adminId});
+      admin.blockedUsers += 1;
+      admin.save();
+  
+      res.status(200).json(admin);
+      console.log(admin);
+  
+    } catch (error) {
+      res.status(400).json(error.message);
+      console.log(error.message);
+    }
+  };
 

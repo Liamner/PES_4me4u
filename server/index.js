657a7c3610c0fd222acd34adf5f25954ca7fdfc6
T0nete <tonbarramon@gmail.com>
2022-01-10 17:23:35 +0100

    no message

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ require('./config/config.js');
 require('dotenv').config();
  
 const app = express()
-const path = require('path');
 
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -16,57 +15,6 @@ const apiRoutes =  require('./routes/api.js')(app);
 app.use("/api", apiRoutes);
 
 app.use(cors());
-
-// Sockets
-const socketIO = require('socket.io')
-const http = require('http')
-const server = http.createServer(app)
-
-const io = socketIO(server)
-
-io.on('connection', (socket) => {
-  // Connect User
-  console.log('user connected')
-  // take userId and socketId from user
-  // necesitamos id del User
-  socket.on('newUser', userId => {
-          console.log('New user: ' + userId)
-          //addUser(userId, socket.id);
-          //io.emit('getUsers', users)
-  });
-
-  // Send message
-  socket.on('sendMessage',(text) => {
-      console.log('New Message: ' + text)
-      /*const user = getUser(reciverId);
-      io.to(user.socket).emit('getMessage', {
-          senderId, 
-          text
-      })*/
-  })
-
-  // Disconnect user
-  socket.on('disconnect', ()=> {
-      console.log('User disconnected')
-      //removeUser(socket.id)
-      //io.emit('getUsers', users)
-  })
-})
-
-let users = [];
-
-/*
-const addUser = (userId, socketId) => {
-    !user.some((user)=> user.userId === userId) && user.push({userId, socketId})
-}
-
-const removeUser = (socketId) => {
-    users= user.filter(user=>user.socketId !== socketId)
-}
-
-const getUser = (userId) => {
-    return getUserRewards.find(user=>user.userId === userId)
-}*/
 
 // Connect to MongoDB
 
